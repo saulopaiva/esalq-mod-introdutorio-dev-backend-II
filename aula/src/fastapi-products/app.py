7d993f52b10c1eafaa5f91b4cc1a0867c4cb3c5d
saulo paiva <saulo@webfatorial.com>
2023-11-06 22:53:00 -0300

    feat: update fastapi project

--- aula/src/fastapi-products/app.py
+++ aula/src/fastapi-products/app.py
@@ -7,5 +7,7 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/test")
-async def test(param: int = 0):
-    return {"message": "Hello World 2"}
+async def test(param: str = ""):
+    param = param + param
+    param = param + param
+    return {"message": "Hello World 2: " + param}
